54993fab8f884ce200f9f49b8aa5ce582824d4dd
Jaydn de Graaf <jd@pinc.nz>
2019-11-15 14:11:51 +1300

    updated styles

--- _config.php
+++ _config.php
@@ -1,1 +1,13 @@
 <?php
+
+use SilverStripe\View\SSViewer;
+use SilverStripe\Admin\LeftAndMain;
+// Force use of custom admin theme
+LeftAndMain::config()->set(
+    'admin_themes',
+    [
+        'permanentinc/beautiful:cms-forms',
+        'silverstripe/admin:cms-forms',
+        SSViewer::DEFAULT_THEME
+    ]
+);
